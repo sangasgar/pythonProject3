dc0a62d32f4910fb912e3ee1657ecf0c4fd5b490
garegin <sangas@yandex.ru>
2021-11-15 22:22:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#1
+list = [1, 2, "дом", 2.1, False, [1, 2, 2]]
+
+for i in list:
+    print(type(i))
+
+
+
+#3
+et = {1: 2, 2: 5}
+et.pop(1)
+
+print(et)
+
+et[21] = 8
+
+print(et)
